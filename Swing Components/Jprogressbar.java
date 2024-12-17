cb481d26404a565b842728dcda374ec739936c95
innovator-sh <itzinnovator01@gmail.com>
2024-12-18 02:14:33 +0530

    Updates #11

--- Swing Components/Jprogressbar.java
+++ Swing Components/Jprogressbar.java
@@ -1,6 +1,49 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Jprogressbar {
-    
+public class Jprogressbar extends JFrame {
+    Jprogressbar() {
+        Container co = getContentPane();
+        co.setLayout(new FlowLayout());
+
+        setVisible(true);
+        setSize(400, 400);
+        setTitle("JProgressBar Testing");
+
+        JProgressBar pb1 = new JProgressBar();
+        JProgressBar pb2 = new JProgressBar(0, 100);
+        JProgressBar pb3 = new JProgressBar(SwingConstants.VERTICAL, 0, 100);
+        pb1.setStringPainted(true);
+        pb2.setStringPainted(true);
+        pb2.setString("hello");
+
+        /* Constructors:
+         * JProgressBar()
+         * JProgressBar(int orientation)
+         * JProgressBar(int min, int max)
+         * JProgressBar(int orientation, int min, int max)
+        */
+
+        co.add(pb1); co.add(pb2); co.add(pb3);
+
+        int i=0; 
+        while(i<=100) {
+            pb1.setValue(i);
+            pb2.setValue(i);
+            pb3.setValue(i);
+            
+            i+=20;
+
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {}
+        }
+        
+
+    }
+
+    public static void main(String args[]) {
+        Jprogressbar t = new Jprogressbar();
+    }
+
 }
---
+++ Swing Components/Jslider.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Jslider extends JFrame {
+    Jslider() {
+
+        Container co = getContentPane();
+        co.setLayout(new FlowLayout());
+
+        JSlider js = new JSlider(JSlider.HORIZONTAL, 0, 50, 10);
+
+        JPanel panel = new JPanel();
+        panel.add(js);
+
+        co.add(panel);
+
+    }
+    public static void main(String[] args) {
+        Jslider t = new Jslider();
+        t.setSize(400, 400);
+        t.setVisible(true);
+        t.setTitle("JSlider Program");
+    }
+}
